3c296fd2a90878c7d41da699c37c194d59a0d603
Lemon Sama <burrisc24@gmail.com>
2022-04-25 17:51:56 -0400

    updated styling and added local storage set item

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,52 @@ function displayDay() {
     const today = moment().format("MMM Do YYYY")
     const currentDay = document.querySelector('#currentDay')
     currentDay.append(today)
-    displayTime()
+    generateDay()
 
 }
 
-const timeArray = ["9am", '10am', '11am', '12pm,', '1pm', '2pm', '3pm', '4pm', '5pm']
+const timeArray = ["9am", '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
 
-function displayTime() {
+function generateDay() {
+
+
     for (i = 0; i < timeArray.length; i++) {
+        let id = [i]
+
+
+
         let timeBlockEl = $('<div>')
-        let timeBlockTimeEL = $('<p>')
+        let timeBlockTimeEL = $('<p>').attr('id', id)
         timeBlockTimeEL.text(timeArray[i])
         let timeTextEl = $('<textarea>')
-        let timeButtonEl = $('<button>')
-        timeBlockEl.addClass('col-md')
-        timeBlockTimeEL.addClass('hour')
-        timeTextEl.addClass('description col-md-10 textarea')
-        timeButtonEl.addClass('savBtn')
+        let timeButtonEl = $('<button>').on('click', function () {
+
+            let hourText = $(this).siblings('.textarea').val()
+            localStorage.setItem(id, hourText)
+
+        })
+
+
+
+
+        let buttonSpanEl = $('<span>')
+        timeBlockEl.addClass('row time-block')
+        timeBlockTimeEL.addClass('col-md')
+        timeTextEl.addClass('col-md-10 textarea')
+        timeTextEl.innerHTML = localStorage.getItem(id)
+        timeButtonEl.addClass('col-md saveBtn')
+        buttonSpanEl.addClass('fa-solid fa-floppy-disk fa-large')
         timeContEl.addClass('column')
+        timeButtonEl.append(buttonSpanEl)
         timeBlockEl.append(timeBlockTimeEL, timeTextEl, timeButtonEl)
         timeContEl.append(timeBlockEl)
 
-
     }
 }
+
+
+
+
 displayDay()
+
+
