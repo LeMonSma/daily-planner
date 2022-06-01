b9ea1233376533af51dd85836c958a7db1f92125
Lemon Sama <burrisc24@gmail.com>
2022-05-31 23:13:39 -0400

    color changes based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 let timeContEl = $('#time-block')
+const hour = moment().format('HH')
 
 function displayDay() {
     const today = moment().format("MMM Do YYYY")
+
     const currentDay = document.querySelector('#currentDay')
     currentDay.append(today)
     generateDay()
 
 }
 
-const timeArray = ["9am", '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+const timeArray = ["9", '10', '11', '12', '13', '14', '15', '16', '17']
 
 function generateDay() {
 
@@ -22,23 +24,36 @@ function generateDay() {
         let timeBlockTimeEL = $('<p>').attr('id', id)
         timeBlockTimeEL.text(timeArray[i])
         let timeTextEl = $('<textarea>')
+        timeTextEl.addClass(id)
         let timeButtonEl = $('<button>').on('click', function () {
 
             let hourText = $(this).siblings('.textarea').val()
             localStorage.setItem(id, hourText)
 
         })
+        if (hour > timeArray[i]) {
+            timeTextEl.addClass('past')
+        } else if (hour === timeArray.values()) {
+            timeTextEl.addClass('now')
 
+        } else {
+            timeTextEl.addClass('future')
+        }
+
+
+
+
+        console.log(timeArray[i])
 
 
 
         let buttonSpanEl = $('<span>')
         timeBlockEl.addClass('row time-block')
-        timeBlockTimeEL.addClass('col-md')
-        timeTextEl.addClass('col-md-10 textarea')
-        timeTextEl.innerHTML = localStorage.getItem(id)
-        timeButtonEl.addClass('col-md saveBtn')
-        buttonSpanEl.addClass('fa-solid fa-floppy-disk fa-large')
+        timeBlockTimeEL.addClass('col-md-1')
+        timeTextEl.addClass(`col-md-10 textarea ${id}`)
+        timeTextEl.val(localStorage.getItem(i))
+        timeButtonEl.addClass('col-md-1 saveBtn')
+        buttonSpanEl.addClass('glyphicon glyphicon-floppy-disk')
         timeContEl.addClass('column')
         timeButtonEl.append(buttonSpanEl)
         timeBlockEl.append(timeBlockTimeEL, timeTextEl, timeButtonEl)
@@ -46,7 +61,6 @@ function generateDay() {
 
     }
 }
-
 
 
 
